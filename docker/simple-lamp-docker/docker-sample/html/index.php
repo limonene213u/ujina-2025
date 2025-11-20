f42904b731fb1bea532fb24bf468b0529c19924d
limonene213u <limonene@hirohsima-aktk.com>
2025-11-20 15:10:43 +0900

    アップデートしました

---
+++ docker/simple-lamp-docker/docker-sample/html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lan="ja">
+<head>
+    <meta charset="utf-8">
+    <title>どっかーどかーん</title>
+    <meta name="description" conten="サイトの概要を記載">
+</head>
+<body>
+    <h1>どっかーが wwwwwwwwwww</h1>
+    <?php
+    echo "どっかーん";
+    ?>
+</body>
+</html>
